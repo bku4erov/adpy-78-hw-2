accab33f020631d6021e43b661c97d1db01f958c
Boris Kucherov <boris.ku4erov@gmail.com>
2023-07-18 20:27:36 +0300

    add merge unique records

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from pprint import pprint
 with open("phonebook_raw.csv", encoding='utf8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-pprint(contacts_list)
-print()
+# pprint(contacts_list)
+# print()
 
 
 ## 1. Выполните пункты 1-3 задания.
@@ -18,12 +18,13 @@ contacts_pattern_replace = r"\g<lastname>,\g<firstname>,\g<surname>,\g<organizat
 re_contacts = re.compile(contacts_pattern)
 
 contacts_list_formatted = [re_contacts.sub(contacts_pattern_replace, ','.join(contact)).split(',') for contact in contacts_list]
-pprint(contacts_list_formatted)
-print()
+# pprint(contacts_list_formatted)
+# print()
+
 
 # Приводим все телефоны в формат +7(999)999-99-99. Если есть добавочный номер, то формат: +7(999)999-99-99 доб.9999.
 phone_with_ad_pattern = r"(\+7|8)?[\s\(]*(\d{3})[\)\s\-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})[\sдоб\.\(]+(\d*)\)*"
-phone_with_ad_pattern_replace = r"+7(\2)\3-\4-\5 доб. \6"
+phone_with_ad_pattern_replace = r"+7(\2)\3-\4-\5 доб.\6"
 phone_pattern = r"(\+7|8)?[\s\(]*(\d{3})[\)\s\-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})"
 phone_pattern_replace = r"+7(\2)\3-\4-\5"
 
@@ -37,13 +38,28 @@ for i, contact in enumerate(contacts_list_formatted):
     else:
         contacts_list_formatted[i][5] = re_phone.sub(phone_pattern_replace, phone)
 
-pprint(contacts_list_formatted)
+# pprint(contacts_list_formatted)
 
 
-## 2. Сохраните получившиеся данные в другой файл.
-## Код для записи файла в формате CSV:
+# Объединяем все дублирующиеся записи о человеке в одну
+# При этом примем следующие допущения:
+# - фамилия в нашем списке уникальные (хотя можно было бы взять уникальным сочетание фамилия+имя (т.к. отчество может быть не указано))
+# - если в дублирующихся записях заполнено одно и то же поле, то информацию берем из наибольшего поля (считая, что там более полная информация)
+contacts_list_unique = {}
+for contact in contacts_list_formatted:
+    if contacts_list_unique.get(contact[0]) is None:
+        contacts_list_unique[contact[0]] = contact
+    else:
+        for i, field in enumerate(contact):
+            contacts_list_unique[contact[0]][i] = max(contacts_list_unique[contact[0]][i], field)
+
+# pprint(contacts_list_unique)
+
+
+# 2. Сохраните получившиеся данные в другой файл.
+## Код для записи файла вcontact формате CSV:
 with open("phonebook.csv", "w", encoding='utf8', newline='') as f:
     datawriter = csv.writer(f, delimiter=',')
 
 ## Вместо contacts_list подставьте свой список:
-    datawriter.writerows(contacts_list_formatted)
+    datawriter.writerows(contacts_list_unique.values())
